881f48bb3350393776bbbe554bfaed3d68cb2ba6
Mahek shah <149513184+mahekshah429@users.noreply.github.com>
2025-06-02 22:30:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your code here
+const input = document.getElementById("evaluatedText");
+const countDisplay = document.getElementById("letterCount");
+
+input.addEventListener("input", () => {
+  countDisplay.textContent = input.value.length;
+});
